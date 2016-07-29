708379ded0755c999859423dbb1565d4829f12dd
zelotoj <zelotoj@gmail.com>
2016-07-29 16:33:25 +0800

    update zelotoj.route.make-image.sh

--- zelotoj.route/upload-files/root/gfwlist2dnsmasq.py
+++ zelotoj.route/upload-files/root/gfwlist2dnsmasq.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#coding=utf-8
+# coding=utf-8
 #
 # Generate a list of dnsmasq rules with ipset for gfwlist
 # Ref https://github.com/gfwlist/gfwlist
@@ -13,27 +13,27 @@ import shutil
 import ssl
 
 mydnsip = '127.0.0.1'
-mydnsport = '5353'
+mydnsport = '2053'
 ipsetname = 'gfwlist'
 # Extra Domain;
-EX_DOMAIN=[ \
-'.google.com', \
-'.google.com.hk', \
-'.google.com.tw', \
-'.google.com.sg', \
-'.google.co.jp', \
-'.google.co.kr', \
-'.blogspot.com', \
-'.blogspot.sg', \
-'.blogspot.hk', \
-'.blogspot.jp', \
-'.blogspot.kr', \
-'.gvt1.com', \
-'.gvt2.com', \
-'.gvt3.com', \
-'.1e100.net', \
-'.blogspot.tw' \
-]
+EX_DOMAIN = [ \
+    '.google.com', \
+    '.google.com.hk', \
+    '.google.com.tw', \
+    '.google.com.sg', \
+    '.google.co.jp', \
+    '.google.co.kr', \
+    '.blogspot.com', \
+    '.blogspot.sg', \
+    '.blogspot.hk', \
+    '.blogspot.jp', \
+    '.blogspot.kr', \
+    '.gvt1.com', \
+    '.gvt2.com', \
+    '.gvt3.com', \
+    '.1e100.net', \
+    '.blogspot.tw' \
+    ]
 
 # the url of gfwlist
 baseurl = 'https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt'
@@ -45,14 +45,14 @@ tmpfile = '/tmp/gfwlisttmp'
 outfile = '/tmp/dnsmasq_list.conf'
 rulesfile = './dnsmasq_list.conf'
 
-fs =  file(outfile, 'w')
+fs = file(outfile, 'w')
 fs.write('# gfw list ipset rules for dnsmasq\n')
 fs.write('# updated on ' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '\n')
 fs.write('#\n')
 
 print 'fetching list...'
 if hasattr(ssl, '_create_unverified_context'):
-	ssl._create_default_https_context = ssl._create_unverified_context
+    ssl._create_default_https_context = ssl._create_unverified_context
 content = urllib2.urlopen(baseurl, timeout=15).read().decode('base64')
 
 # write the decoded content to file then read line by line
@@ -66,34 +66,33 @@ print 'page content fetched, analysis...'
 # remember all blocked domains, in case of duplicate records
 domainlist = []
 
-
 for line in tfs.readlines():
-	if re.findall(comment_pattern, line):
-		print 'this is a comment line: ' + line
-		#fs.write('#' + line)
-	else:
-		domain = re.findall(domain_pattern, line)
-		if domain:
-			try:
-				found = domainlist.index(domain[0])
-				print domain[0] + ' exists.'
-			except ValueError:
-				print 'saving ' + domain[0]
-				domainlist.append(domain[0])
-				fs.write('server=/.%s/%s#%s\n'%(domain[0],mydnsip,mydnsport))
-				fs.write('ipset=/.%s/%s\n'%(domain[0],ipsetname))
-		else:
-			print 'no valid domain in this line: ' + line
+    if re.findall(comment_pattern, line):
+        print 'this is a comment line: ' + line
+    # fs.write('#' + line)
+    else:
+        domain = re.findall(domain_pattern, line)
+        if domain:
+            try:
+                found = domainlist.index(domain[0])
+                print domain[0] + ' exists.'
+            except ValueError:
+                print 'saving ' + domain[0]
+                domainlist.append(domain[0])
+                fs.write('server=/.%s/%s#%s\n' % (domain[0], mydnsip, mydnsport))
+                fs.write('ipset=/.%s/%s\n' % (domain[0], ipsetname))
+        else:
+            print 'no valid domain in this line: ' + line
 
 tfs.close()
 
 for each in EX_DOMAIN:
-	fs.write('server=/%s/%s#%s\n'%(each,mydnsip,mydnsport))
-	fs.write('ipset=/%s/%s\n'%(each,ipsetname))
+    fs.write('server=/%s/%s#%s\n' % (each, mydnsip, mydnsport))
+    fs.write('ipset=/%s/%s\n' % (each, ipsetname))
 
 print 'write extra domain done'
 
-fs.close();
+fs.close()
 print 'moving generated file to dnsmasg directory'
 shutil.move(outfile, rulesfile)
 
--- zelotoj.route/zelotoj.route.make-image.sh
+++ zelotoj.route/zelotoj.route.make-image.sh
@@ -25,77 +25,13 @@ if [ -d $BUILDER_DIR ]; then
     fi
     sed 's/23552k(ubi)/121856k(ubi)/g' target/linux/ar71xx/image/Makefile.bak > target/linux/ar71xx/image/Makefile
 
-    # Make config files
-    mkdir -p $SCRIPTDIR/upload-files/etc/opkg
-    cat > $SCRIPTDIR/upload-files/etc/opkg/distfeeds.conf <<FEEDS_EOF
-#src/gz chaos_calmer_base http://downloads.openwrt.org/chaos_calmer/15.05.1/ar71xx/nand/packages/base
-#src/gz chaos_calmer_luci http://downloads.openwrt.org/chaos_calmer/15.05.1/ar71xx/nand/packages/luci
-#src/gz chaos_calmer_packages http://downloads.openwrt.org/chaos_calmer/15.05.1/ar71xx/nand/packages/packages
-#src/gz chaos_calmer_routing http://downloads.openwrt.org/chaos_calmer/15.05.1/ar71xx/nand/packages/routing
-#src/gz chaos_calmer_telephony http://downloads.openwrt.org/chaos_calmer/15.05.1/ar71xx/nand/packages/telephony
-#src/gz chaos_calmer_management http://downloads.openwrt.org/chaos_calmer/15.05.1/ar71xx/nand/packages/management
-
-src/gz chaos_calmer_base http://23.83.236.66:1081/chaos_calmer/15.05.1/ar71xx/nand/packages/base
-src/gz chaos_calmer_luci http://23.83.236.66:1081/chaos_calmer/15.05.1/ar71xx/nand/packages/luci
-src/gz chaos_calmer_packages http://23.83.236.66:1081/chaos_calmer/15.05.1/ar71xx/nand/packages/packages
-src/gz chaos_calmer_routing http://23.83.236.66:1081/chaos_calmer/15.05.1/ar71xx/nand/packages/routing
-src/gz chaos_calmer_telephony http://23.83.236.66:1081/chaos_calmer/15.05.1/ar71xx/nand/packages/telephony
-src/gz chaos_calmer_management http://23.83.236.66:1081/chaos_calmer/15.05.1/ar71xx/nand/packages/management
-FEEDS_EOF
-
-    mkdir -p $SCRIPTDIR/upload-files/root
-    cat > $SCRIPTDIR/upload-files/root/ss-install.sh <<INS_EOF
-opkg install shadowsocks-libev-spec-polarssl_2.4.8-2_ar71xx.ipk
-
-cat > /etc/init.d/shadowsocks << SS_EOF
-#!/bin/sh /etc/rc.common
-
-START=95
-
-SERVICE_USE_PID=1
-SERVICE_WRITE_PID=1
-SERVICE_DAEMONIZE=1
-IPSET_NAME=$IPSET_NAME
-CONFIG=/etc/shadowsocks.json
-
-start() {
-	#service_start /usr/bin/ss-local -c \$CONFIG -b 0.0.0.0
-	service_start /usr/bin/ss-redir -c \$CONFIG -A -u -f /var/run/ss-redir.pid
-	service_start /usr/bin/ss-tunnel -c \$CONFIG -A -u -l 2053 -L 8.8.8.8:53 -f /var/run/ss-tunnel.pid
-
-        ipset flush \$IPSET_NAME || ipset -N \$IPSET_NAME iphash
-        iptables -t nat -A PREROUTING -p tcp -m set --match-set \$IPSET_NAME dst -j REDIRECT --to-port 1081
-        iptables -t nat -A OUTPUT -p tcp -m set --match-set \$IPSET_NAME dst -j REDIRECT --to-port 1081
-}
-
-stop() {
-        iptables -t nat -D PREROUTING -p tcp -m set --match-set \$IPSET_NAME dst -j REDIRECT --to-port 1081
-        iptables -t nat -D OUTPUT -p tcp -m set --match-set \$IPSET_NAME dst -j REDIRECT --to-port 1081
-        ipset flush \$IPSET_NAME
-
-	service_stop /usr/bin/ss-local
-	service_stop /usr/bin/ss-redir
-	service_stop /usr/bin/ss-tunnel
-}
-SS_EOF
-
-grep -q 'conf-dir=/etc/dnsmasq.d' /etc/dnsmasq.conf || echo 'conf-dir=/etc/dnsmasq.d' >> /etc/dnsmasq.conf
-mkdir -p /etc/dnsmasq.d
-cd /etc/dnsmasq.d
-# Make file gfwlist.txt
-wget --no-check-certificate https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt -O gfwlist.txt
-
-# Make file gfwlist2dnsmasq.py
-python gfwlist2dnsmasq.py
-INS_EOF
-
     # Make WNDR4300 Image
     cd $SCRIPTDIR/$BUILDER_DIR
     BASE="luci luci-theme-bootstrap luci-i18n-base-zh-cn"
     TOOLS="nano bind-dig"
     APPS="luci-i18n-ddns-zh-cn luci-i18n-wol-zh-cn luci-i18n-upnp-zh-cn luci-i18n-qos-zh-cn luci-i18n-commands-zh-cn"
 
-    GFW="-dnsmasq dnsmasq-full ipset iptables-mod-nat-extra"
+    GFW="-dnsmasq dnsmasq-full ip ipset iptables-mod-nat-extra"
     SDK="python python-pip"
     SMB="luci-app-samba luci-app-hd-idle kmod-nls-utf8 kmod-usb-ohci kmod-usb-storage kmod-usb-storage-extras kmod-usb-uhci"
 
